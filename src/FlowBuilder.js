00033263590f3f09a071cc88fa6e6bdcf106bc40
Qiqi Wang <qiqi.wang@gmail.com>
2022-09-28 15:02:46 +0000

    Added FlowCanvas

--- src/FlowBuilder.js
+++ src/FlowBuilder.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
+import FlowCanvas from "./FlowCanvas"
 import "./styles.css";
 
 export default function FlowBuilder(props) {
-  const [vortices, setVortices] = useState([]);
-  return (
-    <div className="FlowBuilder">
-        <h2>Start editing to see some magic happen!</h2>
-      <canvas {...props}/>
-      
-    </div>
-  );
+    const [vortices, setVortices] = useState([]);
+    const canvasSize = [1080, 800];
+    return (
+        <div className="FlowBuilder">
+            <h2>Start editing to see some magic happen!</h2>
+            <FlowCanvas width={canvasSize[0]} height={canvasSize[1]}/>
+        </div>
+    );
 }

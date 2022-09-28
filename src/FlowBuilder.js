1e5f72419fd4b28acbab1ffde2cafcdf4bc8aaa1
Qiqi Wang <qiqi.wang@gmail.com>
2022-09-28 14:17:35 +0000

    Update FlowBuilder.js

--- src/FlowBuilder.js
+++ src/FlowBuilder.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function FlowBuilder(props) {
+  const [vortices, setVortices] = useState([]);
   return (
     <div className="FlowBuilder">
         <h2>Start editing to see some magic happen!</h2>

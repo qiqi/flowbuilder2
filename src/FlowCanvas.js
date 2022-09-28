06ac4eb768b0dd127b1ec0c6526af78f309c7349
Qiqi Wang <qiqi.wang@gmail.com>
2022-09-28 15:03:30 +0000

    Update FlowCanvas.js

--- src/FlowCanvas.js
+++ src/FlowCanvas.js
@@ -2,20 +2,18 @@ import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
 export default function FlowCanvas(props) {
-    let particles = [];
-    const canvasRef = useRef(null);
-    
-    useEffect(() => {
-        setInterval(() => {
-            particles[1] += 1;
-            console.log(particles[1]);
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext('2d');
-            ctx.fillStyle = '#000000';
-            ctx.fillRect(particles[0], particles[1], 10, 10);
-        }, 1000);
-    });
-    return (
-        <canvas ref={canvasRef} {...props}/>
-    );
+  let particles = [100, 100];
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      particles[1] += 1;
+      console.log(particles[1]);
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = "#000000";
+      ctx.fillRect(particles[0], particles[1], 10, 10);
+    }, 1000);
+  });
+  return <canvas ref={canvasRef} {...props} />;
 }
